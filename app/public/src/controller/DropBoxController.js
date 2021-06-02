f98c2ed156cf4fbcdb5b35699bea219d0b637471
Cristiano Azevdo <cristiccorrea@gmail.com>
2021-06-02 17:49:42 -0300

    Barra de progresso funcional

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,8 +3,17 @@ class DropBoxController
     constructor(btnSendfile)
     {
         this.btnSendfileEl = document.querySelector(btnSendfile);
+        // Botão de enviar arquivos
         this.inputFileEL = document.querySelector("#files");
+        //Input Invisivel que força o clik quando clicamos em Enviar
         this.snackModalEl = document.querySelector('#react-snackbar-root');
+        //Modal Barra progresso do upload do nosso arquivo;
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        // barrinha de progresso
+        this.namefileEl = this.snackModalEl.querySelector('.filename');
+        //O nome do arquivo
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
+        //O tempo que o arquivo demorara para fazer o upload
 
         this.initEvents();
     }
@@ -36,15 +45,24 @@ class DropBoxController
                  *  do target e pegando o dados dos 
                  * arquivos selecionados.
                  */
-                
 
-                this.snackModalEl.style.display = 'block';
+                this.modalShow();
+
+                this.inputFileEL.value = '';
+                
                 /**
                  * vamos pegar o elemento que mostra 
                  * a evolução do upload e mostra na
                  * tela .
                  */
         });
+    }
+
+    modalShow(show = true)
+    {
+
+       this.snackModalEl.style.display = (show) ? 'block' : 'none'
+
     }
 
     uploadTask(files/**aqui esta recebendo 
@@ -83,11 +101,13 @@ class DropBoxController
 
                 ajax.onload/** assim descrobiremos
                  se deu errado; */ = event =>
-                {
+                {          
+                    this.modalShow(false)
 
                     try{
                         resolve(JSON.parse(ajax.responseText));
-                    } catch
+                      
+                    } catch(e)
                     {
                         reject(e);
                         /**se o json vier invalido da um erro */
@@ -97,19 +117,42 @@ class DropBoxController
 
                 ajax.onerror = e =>
                 {
+                    this.modalShow(false)
                     reject(e);
                     /** se der algum problema */
                 };
+
+                ajax.upload.onprogress = event =>
+                {
+                    /**a gente quer saber.Já consegui 
+                     * enviar alguns bytes para o 
+                     * servidor avisar a gente enviou
+                     *  mais avisa a gente.Aí a gente 
+                     * consegue fazer a barrinha 
+                     * acontecendo em tempo real.
+                     * Eu não quero um progresso 
+                     * no Ajax como um todo eu quero
+                     *  no upload e ele tem essa
+                     *  inteligência pra isso você
+                     *  coloca  ajax.upload.onprogress
+                     * */
+                     this.uploadProgress(event,file);
+                };
+
+
 
                 let formData = new FormData();
                 /** para ler o arquivo   */
 
                 formData.append('input-file', file)
                 /** esse metodo vai juntar , recebe 
-                 * dois parametros : o nome do cmapo 
-                 * que o post lá no servidor receba ,
+                 * dois parametros : o nome do campo 
+                 * que o post lá no servidor recebe ,
                  * e o segundo parametro que vai receber 
                  * o arquivo que você vai enviar*/
+
+                this.startUploadTime = Date.now();
+                // Captura o inicio do upload 
 
                 ajax.send(formData);
                 /** aqui envia o arquivo */
@@ -120,5 +163,51 @@ class DropBoxController
 
       return Promise.all(promises)
       /** O Promise.all() ele recebe um array de promessas.*/
+    }
+
+    uploadProgress(event,file)
+    {
+       
+       let timespent = Date.now() - this.startUploadTime;
+       /**Essa variavel recebe a subtração da hora de agora menos a hora que iniciou o upload*/ 
+       let loaded = event.loaded;
+       // dados enviados 
+       let total = event.total;
+       //tamanho total do arquivo
+
+       let porcent = parseInt((loaded / total) * 100);
+
+       let timeleft = ((100 - porcent) * timespent) / porcent;
+       // essa variavel garda o tempo restante
+
+       this.progressBarEl.style.width = `${porcent}%`
+
+       this.namefileEl.innerHTML = file.name;
+       this.timeLeftEl.innerHTML = this.formatTimeToHuman(timeleft);
+       
+
+    }
+
+    formatTimeToHuman(duration)
+    {
+
+        
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        
+        if(hours > 0)
+        {
+            return `${hours} horas, ${minutes} e ${seconds} segundos`;
+        }else if (minutes > 0)
+        {
+            return `${minutes} e ${seconds} segundos`
+        }else if (seconds > 0)
+        {
+            return `${seconds} segundos`
+        }
+           
+
     }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload',(req,res)=>
+{
+
+  let form = new formidable.IncomingForm/* recuperando o formulario*/({
+       
+        uploadDir:'./upload',
+        keepExtensions: true
+
+  });
+
+
+  form.parse/**Nesse momento faz o parse 
+   * dos dados que estão vindo*/(req/**os dados*/,(err/**erros*/,fields,files)=>
+   /** esse dois parametros 
+    * ele separa o que são dados
+    *  enviado via post ou se são
+    *  arquivos  */
+  {
+    res.json({
+      files
+      /**nossos dados como parse separou passamos os arquivos */
+    });
+  });
+
+
+
 });
 
 module.exports = router;
